e0044ee2291510103869a60ff4fec0eb7ac52ca9
MarkHeide <118813019+MarkHeide@users.noreply.github.com>
2023-02-15 11:43:55 +0100

    15-2-2023
    I added a player placeholder sprite and put some basic movement in the program, which is not final yet.

--- RhythmGame/OverallLogic/Player.cs
+++ RhythmGame/OverallLogic/Player.cs
@@ -1,7 +1,64 @@
-﻿public class Player : Unit
-{
-    public Player(Vector2 position, Stat hp, Stat ms, string filename, int cols, int rows) : base(position, hp, ms, filename, cols, rows)
-    {
+﻿using GXPEngine;
 
+public class Player : Unit
+{
+    public Player(Vector2 position, Stat hp, Stat ms, string filename, int cols, int rows) : base(position, hp, ms, "Player", 1, 1)
+    {
+        SetOrigin(width / 2, height / 2);
+    }
+
+    void update()
+    {
+        if (Input.GetKey(Key.W))
+        {
+            SetXY(x, y--);
+            
+            if (Input.GetKey(Key.SPACE)) 
+            {
+              for(int i = 0; i <= 7; i ++)
+                {
+                    y--;
+                }
+            }
+        }
+        
+        if (Input.GetKey(Key.A))
+        {
+            SetXY(x--, y);
+
+            if (Input.GetKey(Key.SPACE))
+            {
+                for (int i = 0; i <= 7; i++)
+                {
+                    x--;
+                }
+            }
+        }
+        
+        if (Input.GetKey(Key.S))
+        {
+            SetXY(x, y++);
+
+            if (Input.GetKey(Key.SPACE))
+            {
+                for (int i = 0; i <= 7; i++)
+                {
+                    y++;
+                }
+            }
+        }
+        
+        if (Input.GetKey(Key.D))
+        {
+            SetXY(x++, y);
+
+            if (Input.GetKey(Key.SPACE))
+            {
+                for (int i = 0; i <= 7; i++)
+                {
+                    x++;
+                }
+            }
+        }
     }
 }
